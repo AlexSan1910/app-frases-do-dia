3a179f159c38f9bed2634371d580ad98724fd2d8
AlexSan <alex.gsan11@gmail.com>
2022-05-03 13:51:50 -0300

    atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,32 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "Frases do Dia",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Instagram"),
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Text("Alex Gomes Da Silva Filho"),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.green,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: <Widget>[
-              Text("t1"),
-              Text("t1"),
+void main(List<String> args) {
+  
+}
 
-            ],
-          ),
+class Home extends StatefulWidget{
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia",),
+        backgroundColor: Colors.green[600],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.amber)
+        ),
+        child: Column(
+          children: <Widget>[
+            
+          ],
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
